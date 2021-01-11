50231e91b2055680d192691c8a5c6750e431c7a8
Jack Skiles <jackskiles420@gmail.com>
2021-01-10 19:50:35 -0500

    more cin tests

--- triplex.cpp
+++ triplex.cpp
@@ -3,8 +3,10 @@
 int main()
 {
 
-   int CodeInput;
-   std::cin >> CodeInput;
-   std::cout << CodeInput;
+   int GuessA, GuessB, GuessC;
+   std::cin >> GuessA;
+   std::cin >> GuessB;
+   std::cin >> GuessC;
+   std::cout << GuessA << GuessB << GuessC;
     return 0;
 }
