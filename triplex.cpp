a51cb38fc8c48e4c50ab216c121f47f82f6da5ec
Jack Skiles <jackskiles420@gmail.com>
2021-01-11 10:48:17 -0500

    updated comparison operators

--- triplex.cpp
+++ triplex.cpp
@@ -2,13 +2,24 @@
 
 int main()
 {
+   int CodeA = 10;
+   int CodeB = 15;
+   int CodeC = 63;
 
    int GuessA, GuessB, GuessC;
+
    std::cin >> GuessA;
    std::cin >> GuessB;
    std::cin >> GuessC;
+
    int GuessSum = GuessA + GuessB + GuessC;
+   int CodeSum = CodeA + CodeB + CodeC;
+   int CodeProduct = CodeA * CodeB * CodeC;
    int GuessProduct = GuessA * GuessB * GuessC;
+
+   if(GuessSum == CodeSum && GuessProduct == CodeProduct){
+       std::cout << "You guessed correctly! ";
+   }
    std::cout << GuessSum << " " << GuessProduct;
-    return 0;
+   return 0;
 }
