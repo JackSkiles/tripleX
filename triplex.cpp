07fc9a81af41388260f2f9de7dccadb73dafa6e8
Jack Skiles <jackskiles420@gmail.com>
2021-01-11 11:01:42 -0500

    Added some functions and messages

--- triplex.cpp
+++ triplex.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-int main()
-{
+void IntroMessage(){
+   std::cout << "Welcome! Please enter 3 numbers to guess the sum and product.\n";
+}
+void PlayGame(){
    int CodeA = 10;
    int CodeB = 15;
    int CodeC = 63;
@@ -19,7 +21,16 @@ int main()
 
    if(GuessSum == CodeSum && GuessProduct == CodeProduct){
        std::cout << "You guessed correctly! ";
+   } else {
+       std::cout << "You guessed poorly ";
    }
+
    std::cout << GuessSum << " " << GuessProduct;
+}
+
+int main()
+{
+   IntroMessage();
+   PlayGame();
    return 0;
 }
